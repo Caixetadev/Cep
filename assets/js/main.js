392162c711cb42e2c9cfb62d4c033494eae27e39
Rafael Caixeta <caixetadev@gmail.com>
2021-09-01 22:50:32 -0300

    First Commit

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+const pesquisarCep = async() => {
+  const cep = document.getElementById('cep').value
+  const url = `http://cep.awesomeapi.com.br/json/${cep}`
+
+  const dados = await fetch(url)
+  const endereco = await dados.json()
+  console.log(endereco)
+
+  document.querySelector('section').innerHTML = `
+  
+  <div class="container-cep">
+
+  <h1>CEP ${endereco.cep}</h1>
+  <h1>CEP ${endereco.cep} - ${endereco.address}</h1>
+  
+  <p><b>Endereço:</b> <span>${endereco.address}</span></p>
+  <p><b>Bairro:</b> <span>${endereco.district}</span></p>
+  <p><b>Cidade:</b> <span>${endereco.city}</span></p>
+  <p><b>Estado:</b> <span>${endereco.state}</span></p>
+  <p><b>Endereço Completo:</b><br> <span>${endereco.address}, ${endereco.district}, ${endereco.city}, ${endereco.state}- CEP ${endereco.cep}</span></p>
+  <p><b>Longitude</b>: ${endereco.lng}</p>
+  <p><b>latitude</b>: ${endereco.lat}</p>
+
+
+  </div>
+
+  `
+}
+
+document.getElementById('cep')
+.addEventListener('focusout', pesquisarCep)
+
+
+
+
+
+
